2329525219d0d39dd6fc51c013eeb46d899d0d01
Ratnakirti <44190671+Ratnakirti@users.noreply.github.com>
2018-12-20 19:02:20 +0530

    Initial commit

---
+++ Img Recognition.py
@@ -0,0 +1,27 @@
+import numpy as np
+import matplotlib.pyplot as pt
+import pandas as pd
+from sklearn.tree import DecisionTreeClassifier
+
+data = pd.read_csv("C:/Users/user/Desktop/Image Recognition/Resources/train.csv").values
+clf = DecisionTreeClassifier()
+
+xtrain = data[0:21000,1:]
+train_label = data[0:21000,0]
+
+clf.fit(xtrain,train_label)
+
+xtest = data[21000:,1:]
+actual_label = data[21000:,0]
+
+d = xtest[10]
+d.shape = (28,28)
+pt.imshow(255-d,cmap='gray')
+print(clf.predict([xtest[10]]))
+pt.show()
+
+p = clf.predict(xtest)
+count = 0
+for i in range(0,21000):
+    count+=1 if p[i]==actual_label[i] else 0
+print("Accuracy = ", (count/21000)*100)
